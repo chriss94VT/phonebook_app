44d8d11875c15f0ca3efa281ff8c5b3baeca6153
chriss94VT <chris.shirron@gmail.com>
2019-09-11 11:47:13 -0400

    Add procfile to the project

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 //const morgan = require("morgan");
 const app = express();
 
-//app.use(express.static("build"));
+app.use(express.static("build"));
 app.use(cors());
 app.use(bodyParser.json());
 // app.use(
@@ -39,63 +39,63 @@ app.get("/api/persons", (req, res) => {
   return res.json(entries);
 });
 
-// app.get("/info", (req, res) => {
-//   const content = `<p>Phonebook has info for ${
-//     entries.length
-//   } people</p><p>${new Date()}</p>`;
-//   return res.send(content);
-// });
+app.get("/info", (req, res) => {
+  const content = `<p>Phonebook has info for ${
+    entries.length
+  } people</p><p>${new Date()}</p>`;
+  return res.send(content);
+});
 
-// app.get("/api/persons/:id", (req, res) => {
-//   const id = Number(req.params.id);
-//   const entry = entries.find(e => e.id === id);
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const entry = entries.find(e => e.id === id);
 
-//   if (entry) {
-//     return res.json(entry);
-//   } else {
-//     return res.status(404).end();
-//   }
-// });
+  if (entry) {
+    return res.json(entry);
+  } else {
+    return res.status(404).end();
+  }
+});
 
-// app.delete("/api/persons/:id", (req, res) => {
-//   const id = Number(req.params.id);
-//   entries = entries.filter(e => e.id !== id);
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  entries = entries.filter(e => e.id !== id);
 
-//   console.log(entries);
-//   return res.status(204).end();
-// });
+  console.log(entries);
+  return res.status(204).end();
+});
 
-// app.post("/api/persons", (req, res) => {
-//   const body = req.body;
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
 
-//   const match = entries.find(
-//     entry => entry.name.trim().toLowerCase() === body.name.trim().toLowerCase()
-//   );
+  const match = entries.find(
+    entry => entry.name.trim().toLowerCase() === body.name.trim().toLowerCase()
+  );
 
-//   if (!body.name || !body.number) {
-//     return res.status(400).json({
-//       error: "content not found"
-//     });
-//   } else if (match) {
-//     return res.status(400).json({
-//       error: "name must be unique"
-//     });
-//   }
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: "content not found"
+    });
+  } else if (match) {
+    return res.status(400).json({
+      error: "name must be unique"
+    });
+  }
 
-//   const entry = {
-//     name: body.name,
-//     number: body.number,
-//     date: new Date(),
-//     id: generateID()
-//   };
+  const entry = {
+    name: body.name,
+    number: body.number,
+    date: new Date(),
+    id: generateID()
+  };
 
-//   entries.concat(entry);
-//   return res.json(entry);
-// });
+  entries.concat(entry);
+  return res.json(entry);
+});
 
-// const generateID = () => {
-//   return Math.floor(Math.random() * 1000);
-// };
+const generateID = () => {
+  return Math.floor(Math.random() * 1000);
+};
 
 const PORT = 3001;
 app.listen(PORT, () => {
