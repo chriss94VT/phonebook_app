ab4198a6dc073773b14b20a62fc842b164eba221
chriss94VT <chris.shirron@gmail.com>
2019-09-11 19:26:27 -0400

    uibuild

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 //const morgan = require("morgan");
 const app = express();
 
@@ -22,90 +23,117 @@ app.use(bodyParser.json());
 //     ];
 //   })
 // );
+const url = `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASSWORD}@herokuappdb-eb4ow.mongodb.net/phonebook?retryWrites=true&w=majority`;
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let entries = [
-  {
-    name: "Chris",
-    number: "571-356-9549",
-    id: 1
-  },
-  {
-    name: "James",
-    number: "703-243-6787",
-    id: 2
-  }
-];
+const entrySchema = new mongoose.Schema({
+  name: String,
+  number: String,
+  date: Date,
+  id: Number
+});
+
+const Entry = mongoose.model("Entry", entrySchema);
 
 app.get("/api/persons", (req, res) => {
-  return res.json(entries);
+  Entry.find({})
+    .then(docs => {
+      return res.json(docs);
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 app.get("/info", (req, res) => {
-  const content = `<p>Phonebook has info for ${
-    entries.length
-  } people</p><p>${new Date()}</p>`;
-  return res.send(content);
+  Entry.find({})
+    .then(docs => {
+      const content = `<p>Phonebook has info for ${
+        docs.length
+      } person(s)</p><p>${new Date()}</p>`;
+      return res.send(content);
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const entry = entries.find(e => e.id === id);
-
-  if (entry) {
-    return res.json(entry);
-  } else {
-    return res.status(404).end();
-  }
+  const userID = Number(req.params.id);
+  Entry.findOne({ id: userID })
+    .then(docs => {
+      if (docs === null) {
+        return res.status(404).end();
+      }
+      return res.json(docs);
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  entries = entries.filter(e => e.id !== id);
-  return res.status(204).end();
+  const userID = Number(req.params.id);
+  Entry.findOneAndDelete({ id: userID })
+    .then(docs => {
+      return res.json(docs);
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 app.post("/api/persons", (req, res) => {
   const body = req.body;
 
-  const match = entries.find(
-    entry => entry.name.trim().toLowerCase() === body.name.trim().toLowerCase()
-  );
+  Entry.findOne({ name: /chris/i })
+    .then(docs => {
+      if (docs !== null) {
+        return res.status(400).json({
+          error: "name must be unique"
+        });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+    });
 
   if (!body.name || !body.number) {
     return res.status(400).json({
       error: "content not found"
     });
-  } else if (match) {
-    return res.status(400).json({
-      error: "name must be unique"
-    });
   }
 
-  const entry = {
+  const entry = new Entry({
     name: body.name,
     number: body.number,
     date: new Date(),
     id: generateID()
-  };
+  });
 
-  entries.concat(entry);
+  entry.save().catch(err => {
+    console.log(err);
+  });
   return res.json(entry);
 });
 
 app.put("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
+  const userID = Number(req.params.id);
 
-  const newEntries = entries.map(entry => {
-    if (entry.id === id) {
-      return { ...entry, name: req.body.name, number: req.body.number };
-    }
-    return entry;
-  });
-  return res.send(newEntries);
+  Entry.findOneAndUpdate(
+    { id: userID },
+    { name: req.body.name, number: req.body.number }
+  )
+    .then(docs => {
+      return res.json(docs);
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 const generateID = () => {
-  return Math.floor(Math.random() * 1000);
+  return Math.floor(Math.random() * 100000000);
 };
 
 const PORT = process.env.PORT;
