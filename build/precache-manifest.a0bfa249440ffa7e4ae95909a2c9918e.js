4150d732edbcfca48fe4bd719dd0435697919bd0
chriss94VT <chris.shirron@gmail.com>
2019-09-11 11:05:48 -0400

    Express App commit

---
+++ build/precache-manifest.a0bfa249440ffa7e4ae95909a2c9918e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29149fd7bad0d6293ae7774a914ac237",
+    "url": "/index.html"
+  },
+  {
+    "revision": "465b6acabd6000b67541",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "097be1b31151cf38010f",
+    "url": "/static/js/2.3b6875bd.chunk.js"
+  },
+  {
+    "revision": "465b6acabd6000b67541",
+    "url": "/static/js/main.67ac903a.chunk.js"
+  },
+  {
+    "revision": "2af4fd2b9b1150f37548",
+    "url": "/static/js/runtime~main.59495a5c.js"
+  }
+]);
---
+++ index.js
@@ -0,0 +1,103 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const app = express();
+
+app.use(express.static("build"));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(
+  morgan(function(tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+      JSON.stringify(req.body)
+    ];
+  })
+);
+
+let entries = [
+  {
+    name: "Chris",
+    number: "571-356-9549",
+    id: 1
+  },
+  {
+    name: "James",
+    number: "703-243-6787",
+    id: 2
+  }
+];
+
+app.get("/api/persons", (req, res) => {
+  return res.json(entries);
+});
+
+app.get("/info", (req, res) => {
+  const content = `<p>Phonebook has info for ${
+    entries.length
+  } people</p><p>${new Date()}</p>`;
+  return res.send(content);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const entry = entries.find(e => e.id === id);
+
+  if (entry) {
+    return res.json(entry);
+  } else {
+    return res.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  entries = entries.filter(e => e.id !== id);
+
+  console.log(entries);
+  return res.status(204).end();
+});
+
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
+
+  const match = entries.find(
+    entry => entry.name.trim().toLowerCase() === body.name.trim().toLowerCase()
+  );
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: "content not found"
+    });
+  } else if (match) {
+    return res.status(400).json({
+      error: "name must be unique"
+    });
+  }
+
+  const entry = {
+    name: body.name,
+    number: body.number,
+    date: new Date(),
+    id: generateID()
+  };
+
+  entries.concat(entry);
+  return res.json(entry);
+});
+
+const generateID = () => {
+  return Math.floor(Math.random() * 1000);
+};
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
