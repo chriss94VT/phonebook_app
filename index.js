c2dc8e7e2e513b2ce972841beb22d17b397fa52c
chriss94VT <chris.shirron@gmail.com>
2019-09-11 23:57:27 -0400

    uibuild

--- index.js
+++ index.js
@@ -27,14 +27,14 @@ app.use(bodyParser.json());
 //   })
 // );
 const errorHandler = (error, req, res, next) => {
-  console.error(error.message);
+	console.error(error.message);
 
-  if (error.name === "CastError" && error.kind == "ObjectId") {
-    return res.status(400).send({ error: "malformatted id" });
-  } else if (error.name === "ValidationError") {
-    return res.status(400).json({ error: error.message });
-  }
-  next(error);
+	if (error.name === "CastError" && error.kind === "ObjectId") {
+		return res.status(400).send({ error: "malformatted id" });
+	} else if (error.name === "ValidationError") {
+		return res.status(400).json({ error: error.message });
+	}
+	next(error);
 };
 app.use(errorHandler);
 
@@ -42,112 +42,112 @@ const url = `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASSWORD}@herok
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const entrySchema = new mongoose.Schema({
-  name: {
-    type: String,
-    minlength: 3,
-    required: true
-  },
-  number: {
-    type: String,
-    minlength: 8,
-    required: true
-  },
-  date: Date,
-  id: Number
+	name: {
+		type: String,
+		minlength: 3,
+		required: true
+	},
+	number: {
+		type: String,
+		minlength: 8,
+		required: true
+	},
+	date: Date,
+	id: Number
 });
 
 entrySchema.plugin(uniqueVal);
 const Entry = mongoose.model("Entry", entrySchema);
 
 app.get("/api/persons/", (req, res, next) => {
-  Entry.find({})
-    .then(docs => {
-      return res.json(docs);
-    })
-    .catch(error => next(error));
+	Entry.find({})
+		.then(docs => {
+			return res.json(docs);
+		})
+		.catch(error => next(error));
 });
 
 app.get("/info", (req, res, next) => {
-  Entry.find({})
-    .then(docs => {
-      const content = `<p>Phonebook has info for ${
-        docs.length
-      } person(s)</p><p>${new Date()}</p>`;
-      return res.send(content);
-    })
-    .catch(error => next(error));
+	Entry.find({})
+		.then(docs => {
+			const content = `<p>Phonebook has info for ${
+				docs.length
+			} person(s)</p><p>${new Date()}</p>`;
+			return res.send(content);
+		})
+		.catch(error => next(error));
 });
 
 app.get("/api/persons/:id", (req, res, next) => {
-  const userID = Number(req.params.id);
-  Entry.findOne({ id: userID })
-    .then(docs => {
-      if (docs === null) {
-        return res.status(404).end();
-      }
-      return res.json(docs);
-    })
-    .catch(error => next(error));
+	const userID = Number(req.params.id);
+	Entry.findOne({ id: userID })
+		.then(docs => {
+			if (docs === null) {
+				return res.status(404).end();
+			}
+			return res.json(docs);
+		})
+		.catch(error => next(error));
 });
 
 app.delete("/api/persons/:id", (req, res, next) => {
-  const userID = Number(req.params.id);
-  Entry.findOneAndDelete({ id: userID })
-    .then(docs => {
-      return res.json(docs);
-    })
-    .catch(error => next(error));
+	const userID = Number(req.params.id);
+	Entry.findOneAndDelete({ id: userID })
+		.then(docs => {
+			return res.json(docs);
+		})
+		.catch(error => next(error));
 });
 
 app.post("/api/persons", (req, res, next) => {
-  const body = req.body;
+	const body = req.body;
 
-  if (!body.name || !body.number) {
-    return res.status(400).json({
-      error: "content not found"
-    });
-  }
+	if (!body.name || !body.number) {
+		return res.status(400).json({
+			error: "content not found"
+		});
+	}
 
-  const entry = new Entry({
-    name: body.name,
-    number: body.number,
-    date: new Date(),
-    id: generateID()
-  });
+	const entry = new Entry({
+		name: body.name,
+		number: body.number,
+		date: new Date(),
+		id: generateID()
+	});
 
-  Entry.findOne({ name: body.name })
-    .then(docs => {
-      if (docs !== null) {
-        return res.status(400).json({
-          error: "Unique names are required"
-        });
-      } else {
-        entry.save().catch(error => next(error));
-        return res.json(entry);
-      }
-    })
-    .catch(error => next(error));
+	Entry.findOne({ name: body.name })
+		.then(docs => {
+			if (docs !== null) {
+				return res.status(400).json({
+					error: "Unique names are required"
+				});
+			} else {
+				entry.save().catch(error => next(error));
+				return res.json(entry);
+			}
+		})
+		.catch(error => next(error));
 });
 
 app.put("/api/persons/:id", (req, res, next) => {
-  const userID = Number(req.params.id);
+	const userID = Number(req.params.id);
 
-  Entry.findOneAndUpdate(
-    { id: userID },
-    { name: req.body.name, number: req.body.number }
-  )
-    .then(docs => {
-      return res.json(docs);
-    })
-    .catch(error => next(error));
+	Entry.findOneAndUpdate(
+		{ id: userID },
+		{ name: req.body.name, number: req.body.number }
+	)
+		.then(docs => {
+			return res.json(docs);
+		})
+		.catch(error => next(error));
 });
 
 const generateID = () => {
-  return Math.floor(Math.random() * 100000000);
+	return Math.floor(Math.random() * 100000000);
 };
 
 const unknownEndpoint = (req, res) => {
-  res.status(404).send({ error: "unknown endpoint" });
+	res.status(404).send({ error: "unknown endpoint" });
 };
 app.use(unknownEndpoint);
 
