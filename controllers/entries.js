a2fa331ef30bc2f44dc4cac00451bc2eb6eaeffa
chriss94VT <chris.shirron@gmail.com>
2019-09-17 11:59:16 -0400

    uibuild

--- controllers/entries.js
+++ controllers/entries.js
@@ -3,107 +3,97 @@ const Entry = require("../models/entry");
 const User = require("../models/users");
 
 entriesRouter.get("/", async (req, res, next) => {
-  Entry.find({})
-    .populate("users")
-    .then(docs => {
-      res.json(docs.map(doc => doc.toJSON()));
-    })
-    .catch(error => next(error));
+  try {
+    const entries = await Entry.find({}).populate("user");
+    res.json(entries.map(entry => entry.toJSON()));
+  } catch (error) {
+    next(error);
+  }
 });
 
 entriesRouter.get("/info", async (req, res, next) => {
-  Entry.find({})
-    .then(docs => {
-      const content = `<p>Phonebook has info for ${
-        docs.length
-      } person(s)</p><p>${new Date()}</p>`;
-      res.send(content);
-    })
-    .catch(error => next(error));
+  try {
+    const entries = await Entry.find({});
+    const content = `<p>Phonebook has info for ${
+      entries.length
+    } person(s)</p><p>${new Date()}</p>`;
+    res.send(content);
+  } catch (error) {
+    next(error);
+  }
 });
 
-entriesRouter.get("/:id", (req, res, next) => {
-  const userID = req.params.id;
-  Entry.findOne({
-    _id: userID
-  })
-    .then(docs => {
-      if (docs === null) {
-        res.status(404).end();
-      }
-      res.json(docs);
-    })
-    .catch(error => next(error));
+entriesRouter.get("/:id", async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const user = await Entry.findOne({ _id: id });
+    if (user === null) {
+      res.status(404).end();
+    }
+    res.json(user.toJSON());
+  } catch (error) {
+    next(error);
+  }
 });
 
-entriesRouter.delete("/:id", (req, res, next) => {
-  const userID = req.params.id;
-  Entry.findOneAndDelete({
-    _id: userID
-  })
-    .then(docs => {
-      res.json(docs);
-    })
-    .catch(error => next(error));
+entriesRouter.delete("/:id", async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const entry = await Entry.findOneAndDelete({ _id: id });
+    res.json(entry.toJSON());
+  } catch (error) {
+    next(error);
+  }
 });
 
 entriesRouter.post("/", async (req, res, next) => {
-  const body = req.body;
-  const userObj = await User.findOne({ username: body.username });
+  try {
+    const body = req.body;
+    const userObj = await User.findOne({ username: body.username });
 
-  if (!body.name || !body.number) {
-    return res.status(400).json({
-      error: "content not found"
+    if (!body.name || !body.number) {
+      res.status(400).json({
+        error: "content not found"
+      });
+    }
+
+    const entry = new Entry({
+      name: body.name,
+      number: body.number,
+      date: new Date(),
+      id: generateID(),
+      user: userObj._id
     });
+
+    const duplicate = await Entry.findOne({ name: body.name });
+
+    if (duplicate !== null) {
+      res.status(400).json({
+        error: "Unique names are required"
+      });
+    } else {
+      const newEntry = await entry.save();
+      userObj.entries = userObj.entries.concat(newEntry._id);
+      userObj.save();
+      res.json(entry.toJSON());
+    }
+  } catch (error) {
+    next(error);
   }
-
-  const entry = new Entry({
-    name: body.name,
-    number: body.number,
-    date: new Date(),
-    id: generateID(),
-    user: userObj._id
-  });
-
-  Entry.findOne({
-    name: body.name
-  })
-    .then(docs => {
-      if (docs !== null) {
-        res.status(400).json({
-          error: "Unique names are required"
-        });
-      } else {
-        entry
-          .save()
-          .then(newEntry => {
-            userObj.entries = userObj.entries.concat(newEntry._id);
-            userObj.save();
-          })
-          .catch(error => next(error));
-        res.json(entry);
-      }
-    })
-    .catch(error => next(error));
 });
 
 entriesRouter.put("/:id", async (req, res, next) => {
-  const userID = req.params.id;
-
-  Entry.findOneAndUpdate(
-    {
-      _id: userID
-    },
-    {
-      name: req.body.name,
-      number: req.body.number
-    }
-  )
-
-    .then(docs => {
-      res.json(docs);
-    })
-    .catch(error => next(error));
+  try {
+    const id = req.params.id;
+    const body = req.body;
+    const updatedEntry = await Entry.findOneAndUpdate(
+      { _id: id },
+      { name: body.name, number: body.number }
+    );
+    res.json(updatedEntry.toJSON());
+  } catch (error) {
+    next(error);
+  }
 });
 
 const generateID = () => {
--- controllers/users.js
+++ controllers/users.js
@@ -2,33 +2,29 @@ const bcrypt = require("bcrypt");
 const usersRouter = require("express").Router();
 const User = require("../models/users");
 
-usersRouter.get("/", (req, res, next) => {
-  User.find({})
-    .then(users => {
-      return res.json(users);
-    })
-    .catch(error => {
-      next(error);
-    });
+usersRouter.get("/", async (req, res, next) => {
+  try {
+    const users = await User.find({});
+    res.json(users.map(user => user.toJSON()));
+  } catch (error) {
+    next(error);
+  }
 });
 
-usersRouter.post("/", (req, res, next) => {
-  const body = req.body;
-  const saltRounds = 10;
-  bcrypt
-    .hash(body.password, saltRounds)
-    .then(pwd => {
-      const user = new User({
-        username: body.username,
-        name: body.name,
-        passwordHash: pwd
-      });
-      user.save();
-      return res.json(user);
-    })
-    .catch(error => {
-      next(error);
-    });
+usersRouter.post("/", async (req, res, next) => {
+  try {
+    const body = req.body;
+    const saltRounds = 10;
+    const pwd = await bcrypt.hash(body.password, saltRounds);
+    const user = await new User({
+      username: body.username,
+      name: body.name,
+      passwordHash: pwd
+    }).save();
+    res.json(user.toJSON());
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = usersRouter;
