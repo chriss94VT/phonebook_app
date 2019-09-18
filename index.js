b190e66b8b9d9efe3618b68d8e242ee8748f11e9
chriss94VT <chris.shirron@gmail.com>
2019-09-18 10:47:49 -0400

    uibuild

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const https = require("https");
 const fs = require("fs");
 const config = require("./utils/configUtils");
 
-const creds = {
-  key: fs.readFileSync("./ssl/key.pem", "utf8"),
-  cert: fs.readFileSync("./ssl/cert.pem", "utf8")
-};
-
 if (process.env.NODE_ENV === "development") {
+  const creds = {
+    key: fs.readFileSync("./ssl/key.pem", "utf8"),
+    cert: fs.readFileSync("./ssl/cert.pem", "utf8")
+  };
+
   const httpsServer = https.createServer(creds, app);
 
   httpsServer.listen(config.httpsPort, () => {
