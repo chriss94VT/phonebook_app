5d05675e80d653acac04cdb6035d8afbc05b8025
chriss94VT <chris.shirron@gmail.com>
2019-09-18 10:45:48 -0400

    uibuild

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require("./app");
-// const http = require("http");
+const http = require("http");
 const https = require("https");
 const fs = require("fs");
 const config = require("./utils/configUtils");
@@ -9,13 +9,16 @@ const creds = {
   cert: fs.readFileSync("./ssl/cert.pem", "utf8")
 };
 
-const httpsServer = https.createServer(creds, app);
-// const httpServer = http.createServer(app);
+if (process.env.NODE_ENV === "development") {
+  const httpsServer = https.createServer(creds, app);
 
-httpsServer.listen(config.httpsPort, () => {
-  console.log(`Server running on secure port ${config.httpsPort}`);
-});
+  httpsServer.listen(config.httpsPort, () => {
+    console.log(`Server running on secure port ${config.httpsPort}`);
+  });
+} else {
+  const httpServer = http.createServer(app);
 
-// httpServer.listen(config.httpPort, () => {
-//   console.log(`Server running on port ${config.httpPort}`);
-// });
+  httpServer.listen(config.httpPort, () => {
+    console.log(`Server running on port ${config.httpPort}`);
+  });
+}
