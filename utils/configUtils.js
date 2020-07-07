1e594ae23a00af2708065fe006a5351626af697e
chriss94VT <chris.shirron@gmail.com>
2020-07-07 11:01:44 -0400

    New comments added

--- utils/configUtils.js
+++ utils/configUtils.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 
+// Adding new code to the branch
 const httpPort = process.env.PORT;
 const httpsPort = process.env.SECURE_PORT;
 let url = process.env.DB_URI;
 
+// New comments for commit
 if (process.env.NODE_ENV === "development") {
   url = process.env.DEV_DB_URI;
 } else if (process.env.NODE_ENV === "test") {
